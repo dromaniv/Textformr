da85933b7ddaab802e6567d2cf7acb69189cd79a
aiPatMac <patryk.maciejewski.1@student.put.poznan.pl>
2023-05-20 17:06:11 +0200

    improving inverter class that is includes case positions

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/decorators/Inverter.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/decorators/Inverter.java
@@ -16,8 +16,32 @@ public class Inverter extends Decorator {
 
     @Override
     public String transform(String input) {
-        String transformedInput = transformer.transform(input);
-        StringBuilder result = new StringBuilder(transformedInput).reverse();
-        return result.toString();
+        //inverter should preserve the position of upper and lower case letters
+        //split the input into letters
+        String[] letters = input.split("");
+        //create a list of positions of upper case letters
+        int[] upperCasePositions = new int[letters.length];
+        int upperCaseCount = 0;
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i].equals(letters[i].toUpperCase())) {
+                upperCasePositions[upperCaseCount] = i;
+                upperCaseCount++;
+            }
+        }
+        //lower case all letters
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = letters[i].toLowerCase();
+        }
+        //reverse the order of letters
+        for (int i = 0; i < letters.length / 2; i++) {
+            String temp = letters[i];
+            letters[i] = letters[letters.length - i - 1];
+            letters[letters.length - i - 1] = temp;
+        }
+        //upper case letters in their original positions
+        for (int i = 0; i < upperCaseCount; i++) {
+            letters[upperCasePositions[i]] = letters[upperCasePositions[i]].toUpperCase();
+        }
+        return input;
     }
 }
