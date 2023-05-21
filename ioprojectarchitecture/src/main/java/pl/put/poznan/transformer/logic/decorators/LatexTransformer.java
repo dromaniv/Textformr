da7e099e1aa777f7157a513c1b56fd8836e29112
Dmytro-Romaniv <52633228+Dmytro-Romaniv@users.noreply.github.com>
2023-05-21 22:20:31 +0200

    LatexTransformer documentation

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/decorators/LatexTransformer.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/decorators/LatexTransformer.java
@@ -3,12 +3,25 @@ package pl.put.poznan.transformer.logic.decorators;
 import pl.put.poznan.transformer.logic.AbstractTransformer;
 import pl.put.poznan.transformer.logic.Decorator;
 
+/**
+ * Class used to convert special characters to latex format.
+ * It extends Decorator class.
+ */
 public class LatexTransformer extends Decorator {
 
+    /**
+     * Constructor of LatexTransformer class.
+     * @param transformer AbstractTransformer object to be decorated.
+     */
     public LatexTransformer(AbstractTransformer transformer) {
         super(transformer);
     }
 
+    /**
+     * Method used to convert special characters to latex format.
+     * @param input String to be transformed.
+     * @return String with converted special characters.
+     */
     @Override
     public String transform(String input) {
         String convertedText = replaceSpecialCharacters(input);
